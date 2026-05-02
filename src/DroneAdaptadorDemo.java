372e0a30a500997622fc7245add17533b89621d2
Axthony <axthonyvincere@gmail.com>
2026-05-02 11:29:57 -0500

    feat: Agregue la clase demo DroneAdaptadorDemo

---
+++ src/DroneAdaptadorDemo.java
@@ -0,0 +1,17 @@
+public class DroneAdaptadorDemo {
+    public static void main(String[] args) {
+        SuperDrone superDrone = new SuperDrone();
+        AdaptadorDrone adaptadorDrone = new AdaptadorDrone(superDrone);
+        
+        System.out.println("El dron hace...");
+        superDrone.beep();
+        superDrone.despegar();
+        System.out.println("\nEl Adaptador hace...");
+        testPato(adaptadorDrone);
+    }
+        static void testPato(Pato pato) {
+            pato.cuaquear();
+            pato.volar();
+        }
+
+}
