033920302b89a35277f501870cf586728b49c5e4
Axthony <axthonyvincere@gmail.com>
2026-05-02 11:29:31 -0500

    feat: Agregue la clase AdaptadorDrone con patron Adapter

---
+++ src/AdaptadorDrone.java
@@ -0,0 +1,16 @@
+public class AdaptadorDrone implements Pato {
+    Drone drone;
+
+    public AdaptadorDrone(Drone drone) {
+        this.drone = drone;
+    }
+    @Override
+    public void cuaquear() {
+        drone.beep();
+    }
+    @Override
+    public void volar() {
+        drone.girarRotor();
+        drone.despegar();
+    }
+}
