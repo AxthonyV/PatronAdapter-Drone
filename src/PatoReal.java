5de7bd57f1b0f7ec8812d215f887d9b70c262f55
Axthony <axthonyvincere@gmail.com>
2026-05-02 11:43:15 -0500

    feat: agregar clase PatoReal que implementa Pato

---
+++ src/PatoReal.java
@@ -0,0 +1,12 @@
+public class PatoReal implements Pato {
+    
+    @Override
+    public void cuaquear() {
+        System.out.println("Cuac Cuac");
+    }
+    @Override
+    public void volar() {
+        System.out.println("Estoy volando");
+    }
+
+}
